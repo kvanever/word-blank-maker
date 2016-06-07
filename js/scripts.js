9ecbd1b63399699f2d03d91fd354990ecec995c6
Kevin VanEvery and Leah Nelson <leah@>
2016-06-07 09:19:00 -0700

    add implementation of sxearch using regex in strict violation of instructions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 /*This file is for your custom js.  All yours*/
+$(function () {
+  var sentence = prompt("Enter a sentence please");
+  // var sentenceArray = sentence.split("");
+  // for (var i = 0; i <= sentenceArray.length; i++) {
+  //   if (sentenceArray[i] === "a" || sentenceArray[i] === "e" || sentenceArray[i] === "i" || sentenceArray[i] === "o" || sentenceArray[i] === "u") {
+  //     sentenceArray[i] = "-";
+  //   }
+  // }
+  // var newSentence = sentenceArray.join('');
+  var newSentence = sentence.replace(/[AEIOUaeiou]/ig, "-");
+  alert(newSentence);
+})
